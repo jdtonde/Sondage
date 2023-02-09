92f3f57d7e2e18a1826f6bf2eae2c2ab0a2870ca
Jdtonde <ton2aniel@gmail.com>
2023-02-09 18:01:14 +0000

    instanciation d'application  de sondage avec React

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <div className="App">
+          <h1>Sondage des Dirigeants</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+        <div className="container">
+            <div>Sondage Ui</div>
+        </div>
+    </header>
+  )
+}
+
+export default Header;
