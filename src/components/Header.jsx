5c44375a89fc51e5f5b095bc37935052a4830084
Jdtonde <ton2aniel@gmail.com>
2023-02-09 19:08:14 +0000

    css de base d'application  de sondage avec React

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,40 @@
 import React from 'react'
+import PropTypes from 'prop-types' //pour typer les props recues
 
-function Header() {
+
+function Header({text,bgColor,textColor}) {
+
+    const styles={
+        backgroundColor:bgColor,
+        color:textColor
+    }
   return (
-    <header>
+    <header style={styles}>
         <div className="container">
-            <div>Sondage Ui</div>
+            <div>{text}</div>
         </div>
     </header>
   )
 }
+
+//Add this pour definir tous les types des differents props 
+//Header.propTypes={
+//    props.laprops: PropTypes.typequonveut
+//}
+
+Header.propTypes={
+    text:PropTypes.string,
+    bgColor:PropTypes.string,
+    textColor:PropTypes.string,
+}
+
+Header.defaultProps={
+    text:'Sondage UI',
+    bgColor:'rgba(0,0,0,0.4)',
+    textColor:'#ff6a95'
+}
+
+
+
 
 export default Header;
